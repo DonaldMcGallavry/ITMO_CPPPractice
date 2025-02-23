558b3bdeb560e06aa66f6da553fa59f37e23d9e3
N.Dubskiy <ndubskiy@mail.ru>
2025-02-23 20:27:48 +0300

    create lab tasks from 1 to 5

---
+++ ITMO_CPPPractice_lab3_task4/ITMO_CPPPractice_lab3_task4.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int sum(int n) {
+   
+    if (n == 1) {
+        return 5;
+    }
+    return 5 * n + sum(n - 1);
+}
+
+
+
+int main()
+{
+    int n;
+    cout << "Enter number ";
+    cin >> n;
+    if (n <= 0)
+        cout << "Incorrect number";
+    else
+        cout << "Sum S = 5 + 10 + 15 + … + 5*n where n = " << n << " = " << sum(n) << endl;
+    
+    return 0;
+}
+
---
+++ ITMO_CPPPractice_lab3_task5/ITMO_CPPPractice_lab3_task5.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+string decimalToBinary(int n) {
+    
+    if (n == 0) {
+        return "";
+    }
+    
+    return decimalToBinary(n / 2) + to_string(n % 2);
+}
+
+int main()
+{
+    int n;
+    cout << "Enter number ";
+    cin >> n;
+    cout << "Definition of number " << n << " in binary = " << decimalToBinary(n) << endl;
+    
+    return 0;
+}
+

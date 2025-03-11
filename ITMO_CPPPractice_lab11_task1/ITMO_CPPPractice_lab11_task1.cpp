9fe9fcebe0fab6c8678528987d1e5ef0a7e85d6f
N.Dubskiy <ndubskiy@mail.ru>
2025-03-11 20:49:46 +0300

    Lab11

---
+++ ITMO_CPPPractice_lab11_task1/ITMO_CPPPractice_lab11_task1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include "time.h"
+
+int main() {
+    
+    Time t1(10, 30, 45);
+    Time t2(5, 45, 30);
+
+   
+    Time sum = t1 + t2;
+    std::cout << " t1 + t2: ";
+    sum.print();
+
+    Time diff = t1 - t2;
+    std::cout << " t1 - t2: ";
+    diff.print();
+
+    Time t3 = t1 + 3600.5; 
+    std::cout << "t1 + 3600.5 seconds: ";
+    t3.print();
+
+    Time t4 = 7200.0 + t1;  
+    std::cout << "7200 seconds + t1: ";
+    t4.print();
+
+    if (t1 == t2) {
+        std::cout << "t1 = t2\n";
+    }
+    else if (t1 < t2) {
+        std::cout << "t1 < t2\n";
+    }
+    else {
+        std::cout << "t1 > t2\n";
+    }
+
+    return 0;
+}
